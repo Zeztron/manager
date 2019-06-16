1732cf0ffd9c4b1cd4c0eb30f43b3cf205d9a2cf
Zeztron <pateltharsh@gmail.com>
2019-06-16 19:52:03 -0400

    build login form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import firebase from 'firebase';
+import LoginForm from './components/LoginForm';
+import { Header } from './components/common'
 
 class App extends Component {
 
@@ -23,9 +25,8 @@ class App extends Component {
     render() {
         return (
             <Provider store={createStore(reducers)}>
-                <View>
-                    <Text>Hello</Text>
-                </View>
+                <Header headerText="Managers App"/>
+                <LoginForm />
             </Provider>
         );
     }
